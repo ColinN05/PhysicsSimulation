7269ccd2221cc2ce8e50d16377590f3b80aaac27
unknown <colin.neaville@gmail.com>
2023-01-08 13:12:16 -0600

    still trying to fix language stats . . .

--- b.bat
+++ b.bat
@@ -1,2 +1,2 @@
-g++ src/**.cpp -I src -I glew/include -Lglew -lglew32s -lglew32s -lopengl32 -lgdi32 -o Application
+g++ src/**.cpp -I src -I dependencies/glew/include -Ldependencies/glew -lglew32s -lglew32s -lopengl32 -lgdi32 -o Application
 if NOT %ERRORLEVEL% == 0 PAUSE
